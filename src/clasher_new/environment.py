a4fc8d19ffd5a83f94e5b23abc2e0237fca921a8
jiahaoxiang <jiahaoxiang@mail.ustc.edu.cn>
2026-05-24 20:43:48 +0800

    Refine folder structure so imports work correctly.

--- src/clasher_new/environment.py
+++ src/clasher_new/environment.py
@@ -25,6 +25,10 @@ class CREnv(gym.Env):
         super().__init__()
         self.opponent = opponent_model
         self.battle: battle.BattleState = None
+        self.observation_space = gym.spaces.Dict({
+            "grid": gym.spaces.Box(low=-np.inf, high=np.inf, shape=(32, 18, 15), dtype=np.float32),
+            "hand": gym.spaces.Box(low=0, high=len(entity_names) - 1, shape=(5,), dtype=np.int32)
+        })
 
     def reset(self, *, seed=None, options=None):
         super().reset(seed=seed, options=options)
@@ -33,12 +37,12 @@ class CREnv(gym.Env):
         self.battle = battle.BattleState(player.PlayerState(0, player_0_deck[:], 10),
                        player.PlayerState(1, player_1_deck[:], 10))
         # Now return initial observation
-        pass
+        return self.observe(), {}
 
     def observe(self):
         """Gives a representation of game state"""
         obs = np.zeros((32, 18, 15))
-        for each in self.battle.entities.items():
+        for id, each in self.battle.entities.items():
             entity_id = entity_names.index(each.name)
             player_id = each.player
             elixir = each.data.elixir - 3
@@ -59,6 +63,13 @@ class CREnv(gym.Env):
             obs_arr = np.array([entity_id, player_id, elixir, card_type, speed, is_air, attacks_ground, attacks_air,
                                 hp_left, hp_percentage, hit_speed, attack_range, sight_range, damage, projectile_damage])
             obs[y][x] = obs_arr.copy()
-        return obs
 
+        hand = [entity_names.index(each) for each in self.battle.players[0].cycle[:5]]
 
+        return {
+            'grid': obs,
+            'hand': hand
+        }
+
+if __name__ == '__main__':
+    print(b.players[0].cycle)
